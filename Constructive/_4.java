cc8760c2b4a5e58c7305dc434a0d25201ba73304
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-03-25 09:45:01 +0530

    initial commit

--- Constructive/_4.java
+++ Constructive/_4.java
@@ -1,39 +1,22 @@
-import java.util.Scanner;
-
-public class _44 {
+import java.util.*;
+public class _04 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); 
-        while (t-- > 0) {
-            int n = sc.nextInt(); 
-            String s = sc.next(); 
-            int i = 0, j = n - 1;
-            boolean isValid = true;
-
-            while (i <= j) {
-                char x = s.charAt(i);
-                char y = s.charAt(j);
-
-                char p1 = (x == 'a') ? '1' : (char) (x - 1);
-                char p2 = (y == 'a') ? '1' : (char) (y - 1);
-                char n1 = (x == 'z') ? '2' : (char) (x + 1);
-                char n2 = (y == 'z') ? '2' : (char) (y + 1);
-
-                if (p1 == p2 || p1 == n2 || p2 == n1 || p2 == p1) {
-                    i++;
-                    j--;
-                } else {
-                    isValid = false;
-                    break;
-                }
-            }
-
-
-
-
-            
-            System.out.println(isValid ? "YES" : "NO");
-        }
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int l=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int p=sc.nextInt();
+        int nl=sc.nextInt();
+        int np=sc.nextInt();
         sc.close();
+        int totaldrink=k*l;
+        int totallime=c*d;
+        int totalsalt=p;
+        int ans=Math.min(totalsalt/(n*np),Math.min(totaldrink/(n*nl),totallime/n));
+        System.out.println(ans);
+        
     }
+    
 }
